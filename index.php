5f677d1bc9d5b3c97dcf35b7924296c1e7a4d7f5
lhollow75 <mathilde.deoliveira@gmail.com>
2016-11-15 15:40:32 +0100

    modification de l'index 1.0

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Dare Gorillaz</title>
+		<form action="" method="post">	
+			<input type="hidden" name="l" value="en" />
+			<input type="submit" id="langue_en" value="English">
+		</form>
+		<form action="" method="post">	
+			<input type="hidden" name="l" value="fr" />
+			<input type="submit" id="langue_fr" value="Francais">
+		</form>
+		<?php require_once ('connexionbdd.php'); 
+		session_start();
+		if (!empty($_POST)){
+			$_SESSION['lang']=$_POST['l'];
+		} else {
+			$_SESSION['lang']='fr';
+		}
+		
+		?>
+    </head>
+
+    <body>
+    
+
+<?php
+	function changeLangue($langue){
+		$_SESSION['lang'] = $langue;
+	}
+	
+	$req = $mysql->prepare("SELECT date, heure FROM administration WHERE ville='Paris'");
+	$req->execute();
+	if($req->rowCount()>=1) {
+		while ($donnees = $req->fetch()){
+			$date=$donnees['date'];
+			$heure=$donnees['heure'];
+			$date3=$date.', '.$heure;
+		}
+	}
+	$req2 = $mysql->prepare("SELECT fr, en FROM contenu");
+	$req2->execute();
+	if($req2->rowCount()>=1) {
+		while ($donnees = $req2->fetch()){
+			$affiche[]=$donnees;
+		}
+	}
+	var_dump($affiche);
+	if ($_SESSION['lang']=="fr") echo $affiche[0]['fr']; else echo $affiche[0]['en'];
+	?>
+
+	</br></br>
+<label id="Compte"></label>
+ <script type="text/javascript">
+ var Affiche=document.getElementById("Compte");
+ function Rebour() {
+ var date1 = new Date();
+ var date2 = new Date ('<?php echo $date3; ?>');
+ var sec = (date2 - date1) / 1000;
+ var n = 24 * 3600;
+ if (sec > 0) {
+ j = Math.floor (sec / n);
+ h = Math.floor ((sec - (j * n)) / 3600);
+ mn = Math.floor ((sec - ((j * n + h * 3600))) / 60);
+ sec = Math.floor (sec - ((j * n + h * 3600 + mn * 60)));
+ Affiche.innerHTML = j +" j "+ h +" h "+ mn +" min "+ sec + " s";
+ }
+ tRebour=setTimeout ("Rebour();", 1000);
+ }
+ Rebour();
+ </script>
+ <form action="envoimail.php" id="contact-form" method="post">
+	<p>Dear Erlen,</p>
+	<p>My
+		<label for="your-name">name</label> is
+		<input type="text" name="your-name" id="your-name" minlength="3" placeholder="(your name here)" required> and
+	</p>
+
+	<p>my
+		<label for="email">email address</label> is
+		<input type="email" name="your-email" id="email" placeholder="(your email address)" required>
+	</p>
+
+	<p> I have a
+		<label for="your-message">message</label> for you,
+	</p>
+	<p>
+		<textarea name="your-message" id="your-message" placeholder="(your msg here)" class="expanding" required></textarea>
+	</p>
+	<p>
+		<button type="submit">Envoyer</button>
+  </p>
+</form>
+ <?php 
+ if ($_SESSION['lang']=="fr") echo utf8_encode($affiche[1]['fr']); else echo utf8_encode($affiche[1]['en']);
+ ?>
+    </body>
+</html>
